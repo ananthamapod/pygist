63fe1ecb9852ccc598593d5b25cfc67bc547ca69
Ananth Rao <ananthamapod@gmail.com>
2016-01-29 02:34:46 -0500

    Add logo icon, tests of jquery-based 'scraping'

--- run.js
+++ run.js
@@ -1,3 +1,12 @@
-console.log("Running");
-var py_code = $('pre').innerHTML;
-output = Sk.importMainWithBody("<stdin>", false, py_code, true);
+$(document).ready(function() {
+  // TODO: FIX ME
+  console.log("Running");
+  function test() {
+    var pre = $('pre');
+    var py_code = pre.childNodes;
+    console.log(pre.innerHTML);
+  }
+  setInterval(test, 1000);
+  //Sk.configure({output:console.log});
+  //eval(Sk.importMainWithBody("<stdin>", false, py_code, true));
+});

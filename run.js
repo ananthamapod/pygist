d9df1f58ab0333c15e256bd2306137730ad262aa
David Awad <davidawad64@gmail.com>
2016-01-29 13:28:47 -0500

    more working version

--- run.js
+++ run.js
@@ -1,12 +1,48 @@
+
+function loadXMLDoc(theURL)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari, SeaMonkey
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            console.log('running following github gist');
+            run_python(xmlhttp.responseText);
+
+        }
+    }
+    xmlhttp.open("GET", theURL, false);
+    xmlhttp.send();
+}
+
+function run_python(code){
+    console.log(code);
+
+    Sk.configure({output:console.log});
+
+    eval(Sk.importMainWithBody("<stdin>", false, "print('the python works!')", true));
+}
+
+
+
 $(document).ready(function() {
-  // TODO: FIX ME
-  console.log("Running");
-  function test() {
-    var pre = $('pre');
-    var py_code = pre.childNodes;
-    console.log(pre.innerHTML);
-  }
-  setInterval(test, 1000);
-  //Sk.configure({output:console.log});
-  //eval(Sk.importMainWithBody("<stdin>", false, py_code, true));
+console.log('Welcome to pygist!')
+
+console.log("I'm going to run the code on this page, cool?")
+
+
+loadXMLDoc(window.location.href);
+
+
+console.log(">>>")
+
+
+
 });
